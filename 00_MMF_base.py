a13138b1ec9369175c90debfed746c113d0d20b4
Hannah <20109@apps.eggs.school.nz>
2023-02-23 13:46:16 +1300

    Comments

---
+++ 00_MMF_base.py
@@ -0,0 +1,35 @@
+# import statements
+
+# functions go here
+
+
+# Main Routine
+
+
+# Set up dictionaries / lists needed to hold data
+
+# Get recipe name (can't be blank)
+
+# Loop to get recipe ingredients (end when exit code is typed)
+
+# Get ingredient name
+
+# Get unit of measurement
+
+# Get amount
+
+# Store item as a unit (use list)
+
+# When exit code is typed check that the recipe has at least 2 ingredients and end collection loop
+
+# Get recipe serving size
+
+# Ask for servings desired
+
+# Find scale factor
+
+# Convert relevant ingredients to grams
+
+# Scale ingredients
+
+# Output new, updated ingredient list
