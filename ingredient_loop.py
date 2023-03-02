e244c3a60c15719ae21a143d42742dab43e2c784
Hannah <20109@apps.eggs.school.nz>
2023-03-02 13:39:21 +1300

    Modified Ingredient Loop

--- ingredient_loop.py
+++ ingredient_loop.py
@@ -1,8 +1,11 @@
 ingredients_list = []
 exit_code = "xxx"
 count = 0
-
 valid = True
+
+print("Please enter the names of the ingredients in your recipe. \nDo not include the units or measurements, "
+      "just the name. \nPlease type 'xxx' when you have finished submitting ingredients. ")
+
 while valid:
     ingredient_name = input("Enter an ingredient: ")
 
@@ -12,8 +15,8 @@ while valid:
 
     if ingredient_name == exit_code and count >= 2:
         valid = False
+        print("Here is your list of ingredients:")
         print(ingredients_list)
 
     if ingredient_name == exit_code and count < 2:
-        print("Error")
-
+        print("Error: Please enter at least 2 ingredients")
