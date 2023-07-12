ad86fe17c37930603fcceca821d4884d66da3e28
Hannah <20109@apps.eggs.school.nz>
2023-07-13 09:45:33 +1200

    Bug Fix

--- testing.py
+++ testing.py
@@ -1,10 +1,9 @@
-import csv
-
-ingredients_list = ["flour", "milk", "eggs", "hello", "no", "chicken", "salt", "pepper"]
+import re
+''' ingredients_list = ["flour", "milk", "eggs", "hello", "no", "chicken", "salt", "pepper"]
 units_list = ["cup", "tsp", "", "lb", "tbsp", "g", "g", "g"]
 amount_list = [2, 3, 4, 5, 6, 500, 10, 5]
 converted_units_list = []
-converted_amount_list = []
+converted_amount_list = [1, 5, 10, 0.2]
 ml_list = {
     "mL": 1,
     "L": 1000,
@@ -21,122 +20,159 @@ g_list = {
     "oz": 28.35
 }
 count = 0
+finished_amount_list = [] '''
 
 
-# conversions
-def conversion_check(item):
+''' def string_check(question, answer):
     valid = False
 
     while not valid:
-        with open("/Users/hannah/Downloads/conversions.csv", mode="r") as csvfile:
-            conversions_sheet = csv.DictReader(csvfile)
+        response = input(question).lower()
 
-            if item in ml_list:
-                convert = amount_list[count] * ml_list[item]
-                divide = convert / 250
-
-                try:
-                    for row in conversions_sheet:
-                        if ingredients_list[count] == row["Ingredients"]:
-                            convert = divide * float(row["grams per 250ml"])
-                            converted_units_list.append("g")
-                            return convert
-                finally:
-                    if ingredients_list[count] != row["Ingredients"]:
-                        converted_units_list.append("mL")
-                        return convert
-
-            if item in g_list:
-                convert = amount_list[count] * g_list[item]
-                converted_units_list.append("g")
-                return convert
-
-            if item == "":
-                converted_units_list.append("")
-                return amount_list[count]
-
-
-for x in units_list:
-    converted_amount_list.append(conversion_check(x))
-    count += 1
-
-print(converted_amount_list)
-print(converted_units_list)
-
-
-''' for item in units_list:
-
-    if item in ml_list:
-        convert = amount_list[count] * ml_list[item]
-        divide = convert / 250
-        count += 1
-        print(convert, divide)
-
-        with open("/Users/hannah/Downloads/conversions.csv", mode="r") as csvfile:
-            conversions_sheet = csv.DictReader(csvfile)
-            for x in ingredients_list:
-                for row in conversions_sheet:
-                    if x == row["Ingredients"]:
-
-                        if float(row["grams per 250ml"]) in previous:
-                            break
-
-                        else:
-                            print(row["grams per 250ml"])
-                            conversion = divide * float(row["grams per 250ml"])
-                            converted_amount_list.append(round(conversion, 2))
-                            converted_units_list.append("g")
-                            previous.append(conversion / divide)
-                            print(previous)
-                            print(conversion)
-
-    if item in g_list:
-        convert = amount_list[count] * g_list[item]
-        count += 1
-        print(convert)
-        converted_amount_list.append(convert)
-        converted_units_list.append("g")
-
-    if item == "":
-        ind = units_list.index(item)
-        converted_amount_list.append(amount_list[ind])
-        converted_units_list.append("")
-
-
-print(converted_amount_list)
-print(converted_units_list) '''
-
-# ingredient collection function
-''' ingredients_list = []
-answer = ()
-count = 0
-
-
-def ingredient_loop(question, exit_code):
-    valid = False
-
-    while not valid:
-        ingredient_name = input(question)
-
-        if ingredient_name == exit_code and count >= 2:
-            print(ingredients_list)
-            break
-
-        elif ingredient_name == exit_code and count < 2:
-            print("Error: Please enter at least 2 ingredients")
+        if response in answer:
+            return response
 
         else:
-            count += 1
-            return ingredient_name
+            for thing in answer:
+                if response == thing[0]:
+                    return thing
+
+            print("Error: Please answer with yes or no")
 
 
-while answer != exit_code:
-    answer = ingredient_loop("Enter an ingredient: ", "xxx")
-    count += 1
-    ingredients_list.append(answer) '''
+converted_recipe = []
+method = ()
+want_method = string_check("Would you like to add a method to your new recipe? ", ["yes", "no"])
 
-# regular ingredient collection (done)
-''' def not_blank(question):
+if want_method == "yes":
+    method = input("Enter here: ")
+
+else:
+    pass
+
+if method != ():
+    converted_recipe.append(method)
+
+print(converted_recipe) '''
+
+
+''' def num_check(question):
+    valid = False
+
+    while not valid:
+        response = input(question)
+
+        try:
+            if float(response) <= 0:
+                print("Error: Number must be more than 0")
+
+            else:
+                return response
+
+        except ValueError:
+            print("Error: Please enter a number")
+
+
+ingredients_list = ["flour", "eggs", "milk"]
+amount_list = []
+
+for item in ingredients_list:
+    amount = num_check("Amount of " + item + ": ")
+    amount_list.append(amount)
+print(amount_list) '''
+
+
+''' def ingredient_check(question):
+    valid = False
+    while not valid:
+        item = input(question)
+        number_regex = "^[0.01-100000]"
+        try:
+            amount = float(item[0])
+        except ValueError:
+            print("Error: Please enter a number")
+        unit = item[1]
+        ingredient_name = item[2:]
+
+        if re.match(number_regex, item):
+            pass
+        else:
+            continue
+
+        if amount < 0.01:
+            print("Error: Amount is too small, consider using a different unit")
+
+        elif amount > 100000:
+            print("Error: Amount is too large, consider using a different unit")
+
+        else:
+            print("Error")
+
+        if unit not in valid_unit_list:
+            print("Error: Please enter a valid unit")
+        else:
+            connect = [amount, unit, ingredient_name]
+            join = " ".join(connect)
+            return join
+
+
+valid_unit_list = [
+    [""],
+    ["g", "grams", "gram"],
+    ["mg", "milligrams", "milligram"],
+    ["kg", "kilograms", "kilogram"],
+    ["mL", "millilitres", "ml", "mls", "millilitre"],
+    ["L", "litres", "l", "litre"],
+    ["quart", "quarts", "fl qt", "qt", "q"],
+    ["pint", "pints", "fl pt", "pt", "p"],
+    ["cup", "cups", "c"],
+    ["tbsp", "tablespoons", "tablespoon", "tbs"],
+    ["tsp", "teaspoons", "teaspoon"],
+    ["lb", "pounds", "lbs", "pound"],
+    ["stick", "sticks"],
+    ["oz", "ounces", "ounce", "fl oz", "fluid ounces", "fluid ounce"]
+]
+
+ingredient_list = []
+keep_going = False
+while not keep_going:
+    ingredient_list.append(ingredient_check("Enter an ingredient: "))
+    print(ingredient_list) '''
+
+
+def num_check(question):
+    valid = False
+
+    while not valid:
+        response = input(question)
+
+        try:
+            if float(response) <= 0:
+                print("Error: Number must be more than 0")
+
+            else:
+                return response
+
+        except ValueError:
+            print("Error: Please enter a number")
+
+
+def unit_check(question):
+    valid = False
+
+    while not valid:
+        response = input(question).lower()
+
+        for var_list in valid_unit_list:
+            if response in var_list:
+                response = var_list[0]
+                return response
+
+        else:
+            print("Error: Please enter a valid unit")
+
+
+def not_blank(question):
     valid = False
 
     while not valid:
@@ -149,40 +185,61 @@ while answer != exit_code:
             print("Error: Name cannot be blank")
 
 
+def string_check(question, answer):
+    valid = False
+
+    while not valid:
+        response = input(question).lower()
+
+        if response in answer:
+            return response
+
+        else:
+            for thing in answer:
+                if response == thing[0]:
+                    return thing
+
+            print("Error: Please answer with yes or no")
+
+
+valid_unit_list = [
+    [""],
+    ["g", "grams", "gram"],
+    ["mg", "milligrams", "milligram"],
+    ["kg", "kilograms", "kilogram"],
+    ["mL", "millilitres", "ml", "mls", "millilitre"],
+    ["L", "litres", "l", "litre"],
+    ["quart", "quarts", "fl qt", "qt", "q"],
+    ["pint", "pints", "fl pt", "pt", "p"],
+    ["cup", "cups", "c"],
+    ["tbsp", "tablespoons", "tablespoon", "tbs"],
+    ["tsp", "teaspoons", "teaspoon"],
+    ["lb", "pounds", "lbs", "pound"],
+    ["stick", "sticks"],
+    ["oz", "ounces", "ounce", "fl oz", "fluid ounces", "fluid ounce"]
+]
+count = 0
+keep_going = ()
 ingredients_list = []
-exit_code = "xxx"
-count = 0
 
-complete = False
+while keep_going != "no":
+    amount = num_check("Amount: ")
+    unit = unit_check("Unit: ")
 
-while not complete:
-    ingredient_name = not_blank("Enter an ingredient: ")
-
-    if ingredient_name == exit_code and count >= 2:
-        complete = True
-        print(ingredients_list)
-
-    elif ingredient_name == exit_code and count < 2:
-        print("Error: Please enter at least 2 ingredients")
-
-    else:
+    complete = False
+    while not complete:
+        ingredient_name = not_blank("Enter an ingredient: ")
+        ingredients_list.append(ingredient_name)
         count += 1
-        ingredients_list.append(ingredient_name) '''
+        keep_going = string_check("Enter more ingredients? ", ["yes", "no"])
+        complete = True
 
-# units for testing
-''' ingredients_list = ["milk", "sugar", "eggs"]
-converted_units_list = ["mL", "g", ""]
-finished_amount_list = [250, 100, 2]
-converted_recipe = []
-recipe_name = "cheesy pasta"
-count = 0
+        if keep_going == "yes" and count >= 2:
+            complete = True
+            print()
 
-print(recipe_name.title() + "\n\nIngredients:")
+        elif keep_going == "yes" and count < 2:
+            print("Error: Please enter at least 2 ingredients")
 
-while count != len(ingredients_list):
-    converted_recipe.append(str(finished_amount_list[count]) + converted_units_list[count] + " " +
-                            ingredients_list[count])
-    count += 1
-
-finished_list = "\n".join(converted_recipe)
-print(finished_list) '''
+        else:
+            break
